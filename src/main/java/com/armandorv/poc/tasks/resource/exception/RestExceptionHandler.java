24124fa1e6c67c4cdb0b555268f0dd54f1e4825d
armandorv <armando.ramirezvila@kpn.com>
2018-03-15 20:36:31 +0100

    Configured hibernate validation

--- src/main/java/com/armandorv/poc/tasks/resource/exception/RestExceptionHandler.java
+++ src/main/java/com/armandorv/poc/tasks/resource/exception/RestExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.armandorv.poc.tasks.resource.exception;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
@@ -7,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 import com.armandorv.poc.tasks.resource.dto.ErrorDTO;
 
@@ -18,10 +20,16 @@ public class RestExceptionHandler {
 		ErrorDTO errorDTO = new ErrorDTO(UNAUTHORIZED, "Invalid user credentials.", ex.getMessage());
 		return new ResponseEntity<>(errorDTO, UNAUTHORIZED);
 	}
-	
+
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorDTO> handleBadRequestAlertException(Exception ex) {
 		ErrorDTO errorDTO = new ErrorDTO(INTERNAL_SERVER_ERROR, "Internal Server Error.", ex.getMessage());
 		return new ResponseEntity<>(errorDTO, INTERNAL_SERVER_ERROR);
+	}
+	
+	@ExceptionHandler(WebExchangeBindException.class)
+	public ResponseEntity<ErrorDTO> handleWebExchangeBindException(WebExchangeBindException ex) {
+		ErrorDTO errorDTO = new ErrorDTO(BAD_REQUEST, ex.getReason(), ex.getMessage());
+		return new ResponseEntity<>(errorDTO, BAD_REQUEST);
 	}
 }
